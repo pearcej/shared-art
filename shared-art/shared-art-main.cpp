6d7a22455aecaecf2eaf6463203baf92253c19a6
pearcej <jan_pearce@berea.edu>
2019-01-13 10:03:49 -0500

    added main program

---
+++ shared-art/shared-art-main.cpp
@@ -0,0 +1,33 @@
+/* T03: Our Shared Art
+   by <Put your names here.> */  
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char stop;
+
+	cout << "           _____________________________________ " << endl;
+	cout << "         //_________________(o)_________________\\ " << endl;
+	cout << endl;
+	cout << "         || > Hello Students!                   || " << endl;
+	cout << endl;
+	cout << "         || > Welcome again to CSC 236!         || " << endl;
+	cout << "            > We're happy you are here," << endl;
+	cout << "         || > and want to help!                 || " << endl;
+	cout << "                       - Dr. Jan and TAs" << endl;
+	cout << "         ||                                     || " << endl;
+	cout << endl;
+	cout << "         ||_____________________________________|| " << endl;
+	cout << "        /_______________________________________ \\ " << endl;
+	cout << "       /_| _| _| _| _| _| _| _| _| _| _| _| _| _| \\ " << endl;
+	cout << "      /____| _| _| _| _| _| _| _| _| _| _| _| ___| \\ " << endl;		
+	cout << "     /___| _| _| _| _| _| _| _| _| _| _| _| _| ___| \\ " << endl;
+	cout << "    /__| _| _| _| _| _| _| _| _| _| _| _| _| _| ___  \\ " << endl;
+	cout << "   /_| _| _| _| _|______________________|  _| _|_|_|  \\ " << endl;
+	cout << "  /________________/                  \\________________\\ " << endl;
+	cout << " (________________/____________________\\_______________) " << endl;
+
+	cin >> stop; //Keeps window open in some cases
+	return 0;
+}
